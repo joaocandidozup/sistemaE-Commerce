f7a127181c8b7d1d0f05c54363399e7bd88b72a9
joaocandidozup <joao.candido@zup.com>
2025-01-31 11:51:30 -0300

    feat: create custom exception classes

---
+++ src/main/java/com/example/sistemaECommerce/API/exceptions/ClientAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.example.sistemaECommerce.API.exceptions;
+
+public class ClientAlreadyExistsException extends RuntimeException {
+    public ClientAlreadyExistsException(String message) {
+        super(message);
+    }
+}
